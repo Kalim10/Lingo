9d9860fe1a320351d119116c9eb2b010a680bd22
Kalim <kalim.r1989@gmail.com>
2019-07-26 14:36:12 +0200

    tweede commit

--- Lingo.java
+++ Lingo.java
@@ -7,6 +7,7 @@ public class Lingo {
 
     public void spelen(){
         System.out.println("LINGO");
+        System.out.println("Lingo2");
         //speelBord.bordMaken();
         //speelBord.tonen();
 
