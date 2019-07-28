f73f01a1fb9c4333671aba9fe7c604ce04c49138
Kalim <kalim.r1989@gmail.com>
2019-07-28 16:51:25 +0200

    eerste commit

--- LingoSpelen.java
+++ LingoSpelen.java
@@ -1,4 +1,10 @@
 package VoorbereidingHerexamen.h3.Lingo;
 
 public class LingoSpelen {
+    public static void main(String[] args) {
+        System.out.println("Welkom bij lingo typ STOPPEN om te stoppen");
+        Lingo lingo = new Lingo();
+
+        lingo.spelen();
+    }
 }
