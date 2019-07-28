b1bd94b3af36b82c807756bd79059fcd4966c364
Kalim <kalim.r1989@gmail.com>
2019-07-28 15:06:33 +0200

    eerste commit

--- Lingo.java
+++ Lingo.java
@@ -1,16 +1,4 @@
 package VoorbereidingHerexamen.h3.Lingo;
 
 public class Lingo {
-    private int pogingen = 0;
-    SpeelBord speelBord = new SpeelBord();
-    String woord;
-
-    public void spelen(){
-        System.out.println("LINGO");
-        System.out.println("Lingo2");
-        //speelBord.bordMaken();
-        //speelBord.tonen();
-
-
-    }
 }
--- LingoSpelen.java
+++ LingoSpelen.java
@@ -1,8 +1,4 @@
 package VoorbereidingHerexamen.h3.Lingo;
 
 public class LingoSpelen {
-    public static void main(String[] args) {
-        Lingo lingo = new Lingo();
-        lingo.spelen();
-    }
 }
