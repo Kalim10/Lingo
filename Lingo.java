cebf624c44d620f40efebd6da26e93c54450024f
Kalim <kalim.r1989@gmail.com>
2019-08-03 19:42:00 +0200

    tussen

--- Lingo.java
+++ Lingo.java
@@ -15,46 +15,81 @@ public class Lingo {
 
     }
 
+
     public void poging() {
-        if (invoer.charAt(0) == woord.charAt(0)) {
-            System.out.print("1");
-        } else {
-            System.out.print("0");
+        for (int x = 0; x < woord.length(); x++) {
+            if (invoer.charAt(x) == woord.charAt(x)) {
+                System.out.print("1");
+                if(invoer.charAt(0) == woord.charAt(x))
+                    System.out.print("2");
+                else System.out.println("0");
+            }
         }
-        if (invoer.charAt(1) == woord.charAt(1)) {
-            System.out.print("1");
-        } else {
-            System.out.print("0");
-        }
-        if (invoer.charAt(2) == woord.charAt(2)) {
-            System.out.print("1");
-        } else {
-            System.out.print("0");
-        }
-        if (invoer.charAt(3) == woord.charAt(3)) {
-            System.out.print("1");
-        } else {
-            System.out.print("0");
-        }
-        if (invoer.charAt(4) == woord.charAt(4)) {
-            System.out.print("1");
-        } else {
-            System.out.print("0");
-        }
+//        for (int y = 0; y < woord.length(); y++) {
+//            if (invoer.charAt(0) == woord.charAt(y)) {
+//                System.out.print("2");
+//            }
+//        }
+//        for (int z = 0; z < woord.length(); z++) {
+//            if (invoer.charAt(z) != invoer.charAt(z)) {
+//                System.out.print("3");
+//            }
+//        }
     }
+//                //for (int  y = 0; y<woord.length(); y++)
+//                if (invoer.charAt(0) == woord.charAt(x)) {
+//                    System.out.print("2");
+//                  //  for (int z = 0; z<woord.length(); z++) {
+//                        if (woord.charAt(x) == invoer.charAt(x)) {
+//                            continue;
+//                        }
+//                        System.out.print("0");
+//                    }
+//                }
+//            }
+//        }
+//
+//
+//        if (invoer.charAt(0) == woord.charAt(0)) {
+//            System.out.print("1");
+//
+//        } else {
+//            System.out.print("0");
+//        }
+//        if (invoer.charAt(1) == woord.charAt(1)) {
+//            System.out.print("1");
+//        } else {
+//            System.out.print("0");
+//        }
+//        if (invoer.charAt(2) == woord.charAt(2)) {
+//            System.out.print("1");
+//        } else {
+//            System.out.print("0");
+//        }
+//        if (invoer.charAt(3) == woord.charAt(3)) {
+//            System.out.print("1");
+//        } else {
+//            System.out.print("0");
+//        }
+//        if (invoer.charAt(4) == woord.charAt(4)) {
+//            System.out.print("1");
+//        } else {
+//            System.out.print("0");
+//        }
+ //   }
 
     public void pogingDoorEenLoop() {
-        int aantalPogingen =0;
+        int aantalPogingen = 0;
         int maxPogingen = 5;
         boolean nogEenKeerRaden = false;
         while (!invoer.equals(woord)) {
             System.out.println("Typ je 5 letter woord");
             invoer = sc.nextLine();
-            //System.out.println(invoer);
             poging();
             System.out.println();
             aantalPogingen++;
-           // if(aantalPogingen>maxPogingen)
-        } System.out.println("Gefeliciteerd!");
+            // if(aantalPogingen>maxPogingen)
+        }
+        System.out.println("Gefeliciteerd!");
     }
 }
